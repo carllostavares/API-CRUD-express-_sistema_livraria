88c3759b6bc757954985e9118064d795bcef5d70
Carlos Tavares <carlostavares2350@gmail.com>
2023-03-16 15:52:48 -0300

    Adicionando arquivos Server.js - package.json e packege-lock.json junto ao git ignore. Onde o servidsor local já foi criado no server.js.

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require("http") // requerir o metodo htttp
+const port = 3000; /// definir uma porta para o servidor escutar no servidor local
+
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pagina de livros',
+    '/outores': 'Listagem de autores',
+    '/editora': 'Pagina editora',
+    '/sobre' : 'Informacao sobre o Projeto.'
+}
+
+
+const server = http.createServer((req,res) => {
+res.writeHead(200,{'Content-Type': 'text/plain'})//devolve o staatus 200"OK" com o tipo de conteudo que estou mandando.
+res.end(rotas[req.url]);
+})//criando o servidor usando o modulo http para aplicar o metodo creatServer
+// rep => requisição / res => resposta // usando uma funçãoa callback //Toda vez que o servidor http receber um requisição, a função callback será executada.
+
+server.listen(port,() => {
+    console.log(`Servidor escutando em http://localhhost:${port}`)
+})
+
+// USAR O COMANDO: npm instal nodemon@2.0.15 -D
+// para instalar uma biblioteca para rodar o sevidor sempre que salvarmos o arquivo no VSCode e nao precisar ficar rodandao manualmente no terminal.
+// usadmo os -D no final para indicar que é uma dependência de desenvolvimento e nao vai para produção
+
+//depois vai no package.json  e coloca na parte script : "dev": "nodemon server.js"
+//por fim, executa no terminal : npm run dev
