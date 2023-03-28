6aa3a27cf308f9db975b8e19bef4de320466a60d
Carlos Tavares <carlostavares2350@gmail.com>
2023-03-28 12:50:11 -0300

    Finalizando o CRUD com a implementação da requisição PUT e DELETE.

--- src/app.js
+++ src/app.js
@@ -19,8 +19,38 @@ app.get('/',(req,res) => {
 app.get('/livros',(req,res) =>{
     res.status(200).json(livros)
 })
+
+app.get('/livros/:id',(req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
+
 app.post('/livros',(req, res) =>{
     livros.push(req.body);//aqui ele pega o que tem no corpo da requisição e coloca no array com o push
     res.status(201).send('O livro foi cadastrado consucesso !'); //aqui ele enviar uma resposta de confirmação com a codigo 201 junto com o texto da msg
 })
+
+//o ':id' significa quer vai chegar um oparamentro indicando qual o livro sera atualizado.//com o 'req.params.id' ele vai no parametro da  requisição e pega o id// com o ' req.body.titulo' pega o que vem no corpo, body, e colocar na conteudo do id index
+
+app.put('/livros/:id',(req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+app.delete('/livros/:id',(req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index, 1);
+    res.send(`Livro ${id} foi excluido com sucesso !`);
+
+})
+
+
+//function para pegar o a posição do elemento no array 
+function buscaLivro (id){
+    return livros.findIndex(livro => livro.id == id);
+}
+
 export  default app
